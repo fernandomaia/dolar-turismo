beff4877b2033b4446bbaaa8663b3a95b1fab837
Fernando Maia <fernandosouzamaia@gmail.com>
2018-09-15 09:55:28 +0000

    Added else condition for updates variable

--- bot.py
+++ bot.py
@@ -78,6 +78,8 @@ def main():
         if len(updates["result"]) > 0:
             last_update_id = get_last_update_id(updates) + 1
             echo_all(updates)
+        else:
+            print(MainError: updates)
         time.sleep(0.5)
 
 if __name__ == '__main__':
